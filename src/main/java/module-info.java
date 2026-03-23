5a3843d3cdf6b9bc47038764e9b1ae9dd489729a
LeonardoAntonio2026 <20253ds121@utez.edu.mx>
2026-03-23 08:09:28 -0600

    Merge inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.utez2dpacientesjavafxequipo05 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.utez2dpacientesjavafxequipo05 to javafx.fxml;
+    exports com.example.utez2dpacientesjavafxequipo05;
+}
